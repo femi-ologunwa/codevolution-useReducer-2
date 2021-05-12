338e01efdd89e3331531f96bb889b7e65ed2a361
Femi <ologunwafemi@gmail.com>
2021-05-13 00:08:04 +0100

    refactor code to fetch data with useReducer & useEffect

--- src/DataFetchingOne.js
+++ src/DataFetchingOne.js
@@ -1,37 +1,58 @@
-import React, { useState, useEffect } from 'react';
+import React, { useReducer, useEffect } from 'react';
 import axios from 'axios';
 
+//initial state
+const initialState = {
+   loading: true,
+   error: '',
+   post: {},
+};
+
+//reducer function
+const reducer = (state, action) => {
+   switch (action.type) {
+      case 'FETCH_SUCCESS':
+         return {
+            loading: false,
+            error: '',
+            post: action.payload,
+         };
+
+      case 'FETCH_ERROR':
+         return {
+            loading: false,
+            error: 'Something went wrong',
+            post: {},
+         };
+      default:
+         return state;
+   }
+};
+
 function DataFetchingOne() {
-   //define state variables required for data fetching
-   const [loading, setLoading] = useState(true);
-   const [post, setPost] = useState({});
-   const [error, setError] = useState('');
+   const [state, dispatch] = useReducer(reducer, initialState);
 
    //fetching data from api & updating state
    useEffect(() => {
       axios
          .get('https://jsonplaceholder.typicode.com/posts/1')
          .then((response) => {
-            setLoading(false);
-            setPost(response.data);
-            setError(''); //to remove any error message if there was..
+            dispatch({ type: 'FETCH_SUCCESS', payload: response.data });
          })
          .catch((error) => {
-            setLoading(false);
-            setPost({});
-            setError('Something went wrong');
+            dispatch({ type: 'FETCH_ERROR' });
          });
    }, []);
 
-   if (loading) {
+   if (state.loading) {
       return <h3>Loading...</h3>;
    }
 
-   if (error) {
-      return <h3>{error}</h3>;
+   if (state.error) {
+      return <h3>{state.error}</h3>;
    }
 
-   return <h3>{post.title}</h3>;
+   return <h3>{state.post.title}</h3>;
 }
 
 export default DataFetchingOne;
