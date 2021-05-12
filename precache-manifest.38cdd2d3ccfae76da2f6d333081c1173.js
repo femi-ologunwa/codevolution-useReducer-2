7d9a64541eeb2718806c1c6ab065af22a0d951fa
Femi <ologunwafemi@gmail.com>
2021-05-13 00:18:15 +0100

    Updates

---
+++ precache-manifest.38cdd2d3ccfae76da2f6d333081c1173.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da9604fb8cea83f5d42e209c265f650a",
+    "url": "/codevolution-useReducer-2/index.html"
+  },
+  {
+    "revision": "7993f43da0080e7f34c3",
+    "url": "/codevolution-useReducer-2/static/css/main.f5e5865e.chunk.css"
+  },
+  {
+    "revision": "de54f76855a4ac734933",
+    "url": "/codevolution-useReducer-2/static/js/2.e5bbb58b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/codevolution-useReducer-2/static/js/2.e5bbb58b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7993f43da0080e7f34c3",
+    "url": "/codevolution-useReducer-2/static/js/main.5102c2bf.chunk.js"
+  },
+  {
+    "revision": "b7378364554bdf0e4cb6",
+    "url": "/codevolution-useReducer-2/static/js/runtime-main.db83fe31.js"
+  }
+]);
